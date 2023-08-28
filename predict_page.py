6a27ac33410bad98aca61b4d2097d82673e1c4e7
vipin <vipinpt2002@gmail.com>
2023-08-28 15:09:02 +0530

    Small changes

--- predict_page.py
+++ predict_page.py
@@ -5,7 +5,7 @@ import numpy as np
 
 # loading model from pkl file
 def load_model():
-    with open('saved_steps.pkl', 'rb') as file:
+    with open('./saved_steps.pkl', 'rb') as file:
         data = pickle.load(file)
     return data
 
